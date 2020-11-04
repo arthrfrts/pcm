85bb2433092b33acdc510d28b424b231a56023e3
Arthur Freitas <email@arthr.me>
2020-11-04 12:26:58 +0000

    atualiza o formulário de contato

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,52 @@ $(document).ready(function() {
   });
 });
 
+/*
+  Contact form
+*/
+window.addEventListener("DOMContentLoaded", function() {
+
+  // get the form elements defined in your form HTML above
+  
+  var form = document.getElementById("contact_form");
+
+  // Success and Error functions for after the form is submitted
+  
+  function success() {
+    form.reset();
+    alert(`Sua mensagem foi enviada! Vou respondê-la em breve.\n\nArthur.`);
+  }
+
+  function error() {
+    alert(`Ocorreu um problema ao tentar enviar sua mensagem. Por favor, tente novamente em alguns minutos.`);
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
 
 /*
   Responsive videos.
